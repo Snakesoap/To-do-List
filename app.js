6c00097fe1455f652a45a3a6820a722a6812e87b
Princeton <56801842+Snakesoap@users.noreply.github.com>
2020-01-17 11:27:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-Princeton:test123@cluster0-xnkl0.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-Princeton:Lespaul013@cluster0-xnkl0.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = {
   name: String
